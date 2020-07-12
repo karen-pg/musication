5e88387999bcbda14d8bea417f243b9628e0e66a
karen-pg <flower.pg.0713@gmail.com>
2020-07-12 09:32:59 +0900

    viewsディレクトリにTop.vueを作成、viewsのパスを設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Top from "./views/Top.vue";
+import Menu from "./views/Menu.vue";
+import Info from "./views/Info.vue";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  { path: "/", component: Top },
+  { path: "/menu", component: Menu },
+  { path: "/info", component: Info },
+  { path: "*", redirect: "/" },
+];
 
 const router = new VueRouter({
   mode: "history",
