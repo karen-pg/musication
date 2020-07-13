ec924448688d5a8014a7ec4f4b3511889617c5ba
karen-pg <flower.pg.0713@gmail.com>
2020-07-14 08:30:15 +0900

    Top.vueにHeader.vueをインポート

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <h1>MUSICATION</h1>
+  </header>
+</template>
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,4 +1,14 @@
 <template>
-  <div></div>
+  <div>
+  <Header></Header>
+  </div>
 </template>
+<script>
+import Header from '@/components/Header.vue';
+export default{
+  components:{
+    Header
+  }
+}
+</script>
 <style lang="scss" src="@/assets/scss_files/Top.vue_scss/style.scss"></style>
