5443ebe59d348ff558e0d259523f02eb6afe283f
karen-pg <flower.pg.0713@gmail.com>
2020-07-14 08:37:40 +0900

    Home.vueのパスを設定、それぞれのページにnameを設定

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import VueRouter from "vue-router";
 import Top from "@/views/Top.vue";
 import Login from "@/views/Login.vue";
 import Sign from "@/views/Sign.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/", component: Top },
-  { path: "/login", component: Login },
-  { path: "/sign", component: Sign },
+  { path: "/", component: Top, name: "Top"},
+  { path: "/login", component: Login, name: "Login"},
+  { path: "/sign", component: Sign, name: "Sign"},
+  { path: "/home", component: Home, name: "Home"},
   { path: "*", redirect: "/" },
 ];
 
