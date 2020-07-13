435573ce8e45a5762cd6ace0bc0fa7f62c04ca87
karen-pg <flower.pg.0713@gmail.com>
2020-07-14 08:43:19 +0900

    Header.vueにHome、Login、Singのリンクを設定

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
   <header>
     <h1>MUSICATION</h1>
+    <div>
+      <router-link to="/home">始める</router-link>
+      <router-link to="/login">ログイン</router-link>
+      <router-link to="/sign">新規登録</router-link>
+    </div>
   </header>
 </template>
