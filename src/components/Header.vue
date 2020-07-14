753c9a28473afab0299a4565030e9bf5f75db757
karen-pg <flower.pg.0713@gmail.com>
2020-07-15 07:05:59 +0900

    ハンバーガーメニュー(アニメーション無し)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,17 @@
     <router-link to="/">
       <h1 id="header-logo">MUSICATION</h1>
     </router-link>
-    <div id="humberger">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
     <div>
-      <router-link to="/home" class="header-menu">始める</router-link>
-      <router-link to="/login" class="header-menu">ログイン</router-link>
-      <router-link to="/sign" class="header-menu">新規登録</router-link>
+      <div id="humberger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div>
+        <router-link to="/home" class="header-menu">始める</router-link>
+        <router-link to="/login" class="header-menu">ログイン</router-link>
+        <router-link to="/sign" class="header-menu">新規登録</router-link>
+      </div>
     </div>
   </header>
 </template>
@@ -31,13 +33,42 @@ header {
 }
 #header-logo {
   font-size: 0.8em;
+  margin: 0;
   @media screen and (min-width: 415px) {
     font-size: 1em;
   }
   @media screen and (min-width: 960px) {
     font-size: 1.5em;
   }
-  margin: 0;
+}
+#humberger {
+  span {
+    display: block;
+    background-color: $font-color;
+    width: 25px;
+    height: 2px;
+    margin-bottom: 8px;
+    transition: transform 0.3s ease;
+    &:nth-child(3) {
+      margin: 0;
+    }
+    @media screen and (min-width: 415px) {
+      display: none;
+    }
+  }
+}
+.open {
+  span {
+    &:nth-child(1) {
+      transform: translateY(10px) rotate(45deg);
+    }
+    &:nth-child(2) {
+      transform: translateX(-10px) scale(0);
+    }
+    &:nth-child(3) {
+      transform: translateY(-10px) rotate(135deg);
+    }
+  }
 }
 .header-menu {
   display: none;
