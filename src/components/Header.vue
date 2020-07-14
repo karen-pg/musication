32a5486415205b7e14a1da767227dfb6c586eef3
karen-pg <flower.pg.0713@gmail.com>
2020-07-15 06:38:31 +0900

    bodyサニタイズ、ヘッダー(ハンバーガー以外)スタイル完成

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,8 +16,39 @@
   </header>
 </template>
 <style lang="scss" scoped>
+$base-color: #333;
+$font-color: whitesmoke;
 * {
   text-decoration: none;
-  // color: white;
+  color: $font-color;
+}
+header {
+  background-color: $base-color;
+  padding: 10px 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+#header-logo {
+  font-size: 0.8em;
+  @media screen and (min-width: 415px) {
+    font-size: 1em;
+  }
+  @media screen and (min-width: 960px) {
+    font-size: 1.5em;
+  }
+  margin: 0;
+}
+.header-menu {
+  display: none;
+  @media screen and (min-width: 415px) {
+    font-size: 0.8em;
+    margin-right: 20px;
+    display: inline-block;
+  }
+  @media screen and (min-width: 960px) {
+    margin-right: 40px;
+    font-size: 1.3em;
+  }
 }
 </style>
