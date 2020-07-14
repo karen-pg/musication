1f6e41157aa119a8a7c0a0f7d6894ce0a6346a84
karen-pg <flower.pg.0713@gmail.com>
2020-07-14 18:22:05 +0900

    headerにhome、login、signのリンクを設定

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,23 @@
 <template>
   <header>
-    <h1>MUSICATION</h1>
+    <router-link to="/">
+      <h1 id="header-logo">MUSICATION</h1>
+    </router-link>
+    <div id="humberger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
     <div>
-      <router-link to="/home">始める</router-link>
-      <router-link to="/login">ログイン</router-link>
-      <router-link to="/sign">新規登録</router-link>
+      <router-link to="/home" class="header-menu">始める</router-link>
+      <router-link to="/login" class="header-menu">ログイン</router-link>
+      <router-link to="/sign" class="header-menu">新規登録</router-link>
     </div>
   </header>
 </template>
+<style lang="scss" scoped>
+* {
+  text-decoration: none;
+  // color: white;
+}
+</style>
