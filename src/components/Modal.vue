5d2836556b4e13dd5701c83e3a043897a276886d
karen-pg <flower.pg.0713@gmail.com>
2020-07-19 11:38:24 +0900

    Modal削除、Sign.vueとLogin.vueにHeaderとStyle.scssインポート

--- src/components/Modal.vue
+++
@@ -1,1 +0,0 @@
-<template> </template> <style lang="scss" scoped></style>
--- src/views/Sign.vue
+++ src/views/Sign.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
+    <Header>
+      <template v-slot:header-menus>
+        <router-link to="/home" class="header-menu">始める</router-link>
+        <router-link to="/login" class="header-menu">ログイン</router-link>
+        <router-link to="/sign" class="header-menu">新規登録</router-link>
+      </template>
+      <template v-slot:slide-header-menus>
+        <router-link to="/home" class="slide-header-menu">始める</router-link>
+        <router-link to="/login" class="slide-header-menu"
+          >ログイン</router-link
+        >
+        <router-link to="/sign" class="slide-header-menu">新規登録</router-link>
+      </template>
+    </Header>
     <h1>Sign.vue</h1>
   </div>
 </template>
+<script>
+import Header from "../components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+<style
+  lang="scss"
+  src="@/assets/scss_files/Top.vue_scss/style.scss"
+  scoped
+></style>
