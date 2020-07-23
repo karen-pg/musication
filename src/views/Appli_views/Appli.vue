ba39137667b2af51706a87cd8796fc8f2c3a7285
karen-pg <flower.pg.0713@gmail.com>
2020-07-23 16:57:32 +0900

    アプリの上部整形

--- src/views/Appli_views/Appli.vue
+++ src/views/Appli_views/Appli.vue
@@ -6,13 +6,14 @@
       <input type="text" class="search-text" placeholder="曲名、タグなど" />
     </div>
     <div class="search-switches">
-      <router-link :to="{name:'Home'}" class="search-switch">トップ</router-link>
-      <router-link :to="{name:'Popurality'}" class="search-switch">人気</router-link>
-      <router-link :to="{name:'New'}" class="search-switch">新着</router-link>
-      <router-link :to="{name:'Genre'}" class="search-switch">ジャンル</router-link>
-      <router-link :to="{name:'Board'}" class="search-switch">掲示板</router-link>
+      <router-link :to="{name:'Home'}" class="search-switch" active-class="active">トップ</router-link>
+      <router-link :to="{name:'Popurality'}" class="search-switch" active-class="active">人気</router-link>
+      <router-link :to="{name:'New'}" class="search-switch" active-class="active">新着</router-link>
+      <router-link :to="{name:'Genre'}" class="search-switch" active-class="active">ジャンル</router-link>
+      <router-link :to="{name:'Board'}" class="search-switch" active-class="active">掲示板</router-link>
     </div>
   </nav>
+  <hr />
   <router-view></router-view>
 </body>
 </template>
@@ -25,24 +26,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-@import "@/assets/scss_files/_import.scss";
-.container {
-  padding: 20px;
-}
-.search {
-  border-bottom: 1px $sub-font-color;
-  margin-bottom: 20px;
-  &-text {
-    padding: 5px;
-    width: 100%;
-    border-radius: 10px;
-  }
-}
-.search {
-  &-switches {
-    padding: 10px 20px;
-    display: flex;
-    justify-content: space-between;
-  }
-}
+@import "@/assets/scss_files/Appli_scss/Appli.scss";
 </style>
