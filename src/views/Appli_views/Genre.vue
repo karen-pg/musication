23d335119b562a0ec72b2bb4edac50e8ad6865f3
karen-pg <flower.pg.0713@gmail.com>
2020-07-25 10:01:43 +0900

    アイコンのスタイルを_importに移動、Genreのインフォメーション作成

--- src/views/Appli_views/Genre.vue
+++ src/views/Appli_views/Genre.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>ジャンル</h1>
+  <section>
+    <div class="attention">
+      <div class="attention__img">
+        <img src="@/assets/imgs/3438372_m.jpg" alt="マキナ" />
+      </div>
+      <div class="attention__detail">
+        <p>バルセロナ発祥のEDMを体感しよう</p>
+      </div>
+    </div>
+    <div class="container">
+      <h1>
+        人気ジャンル
+        <font-awesome-icon icon="chevron-right" class="icon" />
+      </h1>
+      <div class="genre-wrapper">
+        <div class="ganre"></div>
+      </div>
+    </div>
+  </section>
 </template>
+<style lang="scss" scoped>
+@import "@/assets/scss_files/Appli_scss/Appli-child_scss/Genre_scss/style.scss";
+</style>
