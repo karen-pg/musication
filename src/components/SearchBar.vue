18fc0d0a099421bee660217a78777d9780e101c9
karen-pg <flower.pg.0713@gmail.com>
2020-07-27 11:54:53 +0900

    ヘッダー、ルーターのレスポンシブ完成

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -4,8 +4,9 @@
 <style lang="scss" scoped>
 .search {
   width: 100%;
-  font-size: 18px;
-  padding: 3px;
+  font-size: 13px;
+  padding: 2px;
   border-radius: 10px;
+  border: 1px solid gray;
 }
 </style>
