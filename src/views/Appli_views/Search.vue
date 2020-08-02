6842b7a7252d4cbee6a6c9385847c5367d62ee2e
karen-pg <flower.pg.0713@gmail.com>
2020-08-02 09:08:41 +0900

    searchbar.vueを削除

--- src/views/Appli_views/Search.vue
+++ src/views/Appli_views/Search.vue
@@ -1,19 +1,69 @@
 <template>
   <section>
     <div class="container">
-      <SearchBar></SearchBar>
+      <form action="#">
+        <div class="search">
+          <input type="text" v-model="val" class="search-bar" placeholder="キーワードを入力" />
+          <font-awesome-icon
+            v-show="val.length"
+            @click="clear()"
+            icon="times-circle"
+            class="search-cansel"
+          />
+        </div>
+      </form>
     </div>
   </section>
 </template>
 <script>
-import SearchBar from "../../components/SearchBar.vue";
 export default {
-  components: {
-    SearchBar,
+  data() {
+    return {
+      val: "",
+    };
+  },
+  methods: {
+    clear() {
+      this.val = "";
+    },
   },
 };
 </script>
 <style lang="scss" scoped>
 @import "@/assets/scss_files/_import.scss";
 @import "@/assets/scss_files/Appli_scss/Appli-child_scss/_import.scss";
+.container {
+  padding: 20px;
+}
+.search {
+  position: relative;
+  height: 35px;
+  &-bar {
+    width: 100%;
+    height: 100%;
+    font-size: 15px;
+    padding: 5px;
+    border-radius: 10px;
+    border: 2px solid $sub-font-color;
+    @media screen and (min-width: 960px) {
+      width: 400px;
+    }
+    &::placeholder {
+      color: $sub-font-color;
+    }
+    &:focus {
+      border: 2px solid $red;
+    }
+  }
+  &-cansel {
+    position: absolute;
+    top: 50%;
+    right: 8px;
+    transform: translateY(-50%);
+    color: $sub-font-color;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
 </style>
