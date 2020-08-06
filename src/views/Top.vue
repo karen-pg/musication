940607b36888f44fa01eb2363b2513f7089dff6a
karen-pg <flower.pg.0713@gmail.com>
2020-08-06 10:48:20 +0900

    ホームページのスタイル修正完了、リンクホバー時のアニメーション_importに追加

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,89 +1,95 @@
 <template>
   <div id="body">
     <TopHeader class="header"></TopHeader>
-    <section id="hero">
-      <div id="hero-container">
-        <div id="hero-container-title">
-          <h1>
-            音楽を
-            <br />探究しよう
-          </h1>
+    <div class="sticky-range">
+      <section id="hero">
+        <div id="hero-container" :class="{'fade-animation': fadeAnimation}">
+          <div id="hero-container-title">
+            <h1>
+              音楽を
+              <br />探究しよう
+            </h1>
+            <p>
+              ようこそ、
+              <br />新しい音楽の世界へ。
+            </p>
+          </div>
+        </div>
+      </section>
+      <section id="intro">
+        <h2>
+          音楽を
+          <br />全力で楽しむ
+          <br />プラットフォーム
+        </h2>
+        <div class="explanation" id="intro-explanation">
           <p>
-            ようこそ、
-            <br />新しい音楽の世界へ。
+            MUSICATIONは、
+            創作した音楽の投稿・閲覧ができるプラットフォームコンテンツサービスです。
+          </p>
+          <p>
+            自分の音楽を発信したい、新しい音楽を見つけたい、仲間と繋がりたい ―
+            <br />音楽を様々な形で、全力で楽しみたい人のために。
           </p>
         </div>
-        <hr />
-        <router-link :to="{name:'Home'}" class="start-btn link-btn">始める</router-link>
-      </div>
-    </section>
-    <section id="intro">
-      <h2>
-        音楽の輪を広げる
-        <br />プラットフォーム
-      </h2>
-      <div class="explanation" id="intro-explanation">
-        <p class="strong">
-          MUSICATIONは、
-          創作した音楽の投稿・閲覧ができるプラットフォームコンテンツサービスです。
-        </p>
-      </div>
-    </section>
-    <section id="descriptions">
-      <div id="description1" class="description">
-        <div class="description-container">
-          <div class="description-img">
-            <img
-              src="@/assets/imgs/music-guitar-acoustic-guitar-musician-musical-instrument-bassist-singing-sound-guitarist-drums-bass-guitar-singer-songwriter-string-instrument-plucked-string-instruments-slide-guitar-10690.jpg"
-              alt="ギター"
-            />
-          </div>
-          <div class="description-wrapper explanation">
-            <h3>
-              あなたの
-              <br />音楽を
-              <br />世界へ
-            </h3>
-            <p class="strong">オリジナル楽曲を作成して世界に発信しましょう。</p>
-            <p>あなたのアイディアで新しい音楽を生み出しましょう。</p>
+        <router-link :to="{name: 'Home'}" class="link-btn start-btn">始める</router-link>
+      </section>
+      <section id="descriptions">
+        <div id="description1" class="description">
+          <div class="description-container">
+            <div class="description-img">
+              <img
+                src="@/assets/imgs/music-guitar-acoustic-guitar-musician-musical-instrument-bassist-singing-sound-guitarist-drums-bass-guitar-singer-songwriter-string-instrument-plucked-string-instruments-slide-guitar-10690.jpg"
+                alt="ギター"
+              />
+            </div>
+            <div class="description-wrapper explanation">
+              <h3>
+                あなたの
+                <br />音楽を
+                <br />世界へ
+              </h3>
+              <p class="strong">オリジナル楽曲を作成して世界に発信しましょう。</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div id="description2" class="description">
-        <div class="description-container">
-          <div class="description-img">
-            <img src="@/assets/imgs/utGYwaL0fm.jpg" alt="音楽を聴く女性" />
-          </div>
-          <div class="description-wrapper explanation">
-            <h3>
-              ジャンルは
-              <br />無限大
-            </h3>
-            <p class="strong">ユーザーのアイディアが、新しいジャンルを生み出します。</p>
-            <p>ユーザーが作成した個性あふれる楽曲から、最高の音楽と出会いましょう。</p>
-          </div>
-        </div>
-      </div>
-      <div id="description3" class="description">
-        <div class="description-container">
-          <div class="description-img">
-            <img
-              src="@/assets/imgs/person-music-group-guitar-crowd-concert-musician-barcelona-performance-art-festival-saxophone-performance-singing-guitarist-drummer-entertainment-street-musicians-performing-arts-social-gr.jpg"
-              alt="演奏する二人の男性"
-            />
-          </div>
-          <div class="description-wrapper explanation">
-            <h3>
-              新しい
-              <br />仲間と
-              <br />繋がる
-            </h3>
-            <p class="strong">掲示板で仲間を探すことができます。</p>
-            <p>同じ音楽が好きな仲間はもちろん、一緒に創作活動をする仲間も探すことができます。</p>
+        <div id="description2" class="description">
+          <div class="description-container">
+            <div class="description-img">
+              <img src="@/assets/imgs/utGYwaL0fm.jpg" alt="音楽を聴く女性" />
+            </div>
+            <div class="description-wrapper explanation">
+              <h3>
+                新しい
+                <br />音楽と
+                <br />出会う
+              </h3>
+              <p class="strong">ジャンルから音楽を探すことができます。</p>
+              <p>ユーザーが作成した個性あふれる楽曲から、最高の音楽と出会いましょう。</p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+        <div id="description3" class="description">
+          <div class="description-container">
+            <div class="description-img">
+              <img
+                src="@/assets/imgs/person-music-group-guitar-crowd-concert-musician-barcelona-performance-art-festival-saxophone-performance-singing-guitarist-drummer-entertainment-street-musicians-performing-arts-social-gr.jpg"
+                alt="演奏する二人の男性"
+              />
+            </div>
+            <div class="description-wrapper explanation">
+              <h3>
+                新しい
+                <br />仲間と
+                <br />繋がる
+              </h3>
+              <p class="strong">掲示板で仲間を探すことができます。</p>
+              <p>同じ音楽が好きな仲間はもちろん、一緒に創作活動をする仲間も探すことができます。</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
     <section id="close">
       <div class="close-container">
         <div class="close-text">
@@ -109,6 +115,23 @@ export default {
   components: {
     TopHeader,
     Footer,
+  },
+  data() {
+    return {
+      fadeAnimation: false,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.fadeOut);
+  },
+  methods: {
+    fadeOut() {
+      const intro = document.querySelector("#intro");
+      const introH = intro.getBoundingClientRect().top;
+      introH < window.innerHeight / 2.5
+        ? (this.fadeAnimation = true)
+        : (this.fadeAnimation = false);
+    },
   },
 };
 </script>
